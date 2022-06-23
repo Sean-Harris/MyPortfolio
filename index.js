59ddeae1cb2c9f6e81110f7e207c5cf0c934935a
Seonsean <gameguysean@gmail.com>
2022-06-23 15:15:58 +0300

    attempt to make loading screen work on mobile

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-'use strict';
-
 (function () {
   var $triangles = document.querySelectorAll('.triangle');
   var template = '<svg class="triangle-svg" viewBox="0 0 140 141">\n    <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">\n      <polygon class="triangle-polygon"  points="70 6 136 138 4 138"></polygon>\n    </g>\n  </svg>';
