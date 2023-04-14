be091e006e8f5b4d1523d2d07767ad19f9f17d5f
Seonsean <gameguysean@gmail.com>
2023-04-14 17:59:54 +0300

    0.2.74 - maybemaybemaybe

--- main.js
+++ main.js
@@ -319,7 +319,7 @@ const planeAspectRatio = 16 / 9;
 
 const viewportSize = {
   width: window.innerWidth,
-  height: screen.height//window.height//window.innerHeight
+  height: window.outerHeight//window.height//window.innerHeight
 };
 
 
